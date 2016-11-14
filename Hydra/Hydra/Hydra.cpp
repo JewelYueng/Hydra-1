40f995e2841fc403d8f4c2673c10cc98d672eaee
jason-xuan <zjxuan1996@outlook.com>
2016-11-14 16:26:52 +0800

    add pipe communications

--- Hydra/Hydra/Hydra.cpp
+++ Hydra/Hydra/Hydra.cpp
@@ -2,10 +2,58 @@
 //
 
 #include "stdafx.h"
+#include "pisqpipe.h"
 
-
-int main()
+namespace Hydra
 {
-    return 0;
-}
+	// globals
+	const char *infotext = "name=\"Hydra\", "
+		"author=\jason\", "
+		"version=\"1.0\", "
+		"country=\"China\"";
 
+
+	// pip functions via pisqpipe.h
+	void brain_init()
+	{
+		// init
+		pipeOut("OK");
+	}
+
+	void brain_restart()
+	{
+		// delete old one
+		// create new one
+		pipeOut("OK");
+	}
+
+	void brain_turn()
+	{
+
+	}
+
+	void brain_my(int x, int y)
+	{
+
+	}
+
+	void brain_opponents(int x, int y)
+	{
+
+	}
+
+	void brain_block(int x, int y)
+	{
+
+	}
+
+	int	 brain_takeback(int x, int y)
+	{
+		return 0;
+	}
+
+	void brain_end()
+	{
+
+	}
+}
--- Hydra/Hydra/stdafx.h
+++ Hydra/Hydra/stdafx.h
@@ -5,10 +5,14 @@
 
 #pragma once
 
+#define _CRT_SECURE_NO_DEPRECATE
+
+
 #include "targetver.h"
 
 #include <stdio.h>
 #include <tchar.h>
+
 
 
 
